4139f598484df57bb431bf01b2b9d2d3f4680973
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-08-02 15:31:49 -0500

    questions answered

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -53,21 +60,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: Explain. 
+# The line of code is comparing the number 4 and asking if it is a smaller number than 9, which is true. "Puts" is what is executing the question. 
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+# This line is asking if the number of books (which was defined as the number 3 above) is greater than the number for. Or technically asking if 4 is less than the number of books, which is 3, making this a false statement. 
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+# This line has defined the number of friends as 6, and the number of siblings as 2. It is asking if the number of friends(6) is greater than the number of siblings(2), which is true. 
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+# This line has defined the number of attendees as 9 and the number for meals as 8. It is asking if the number of attendees is not equal to the number of meals available, which is true. 
 
 
 #-------------------
@@ -90,16 +101,19 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age < 2
 # Assume that a dog is a puppy if it is less than 2 years old.
-
+puts age < 2
+    
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: The final 2 lines of code did evaluate to true. I believe that this happened because in the last line, it is assuming that a dog is a puppy if it is 
+# less than 2 years of age, and the age was defined as 1, which makes the statements true, it is a puppy, because it is less than 2 years old. 
